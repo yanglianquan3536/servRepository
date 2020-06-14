c5cc2e469f0e72754f7d6db7a1a6a26df87939dd
1009517319@qq.com <1009517319@qq.com>
2020-06-14 22:47:42 +0800

    架构设计

--- register/src/main/java/com/quang/serv/register/RegisterBootstrap.java
+++ register/src/main/java/com/quang/serv/register/RegisterBootstrap.java
@@ -1,5 +1,8 @@
 package com.quang.serv.register;
 
+import com.quang.serv.pipeline.health.server.HealthServerBootstrap;
+import com.quang.serv.register.health.ServiceHealthCollector;
+
 /**
  * 注册中心应集群部署，避免单点
  * 注册中心通过该类启动，执行main方法即可
@@ -7,6 +10,7 @@ package com.quang.serv.register;
  */
 public class RegisterBootstrap {
     public static void main(String[] args) {
-
+        // 启动健康数据上报
+        HealthServerBootstrap.start(new ServiceHealthCollector());
     }
 }
--- register/src/main/java/com/quang/serv/register/health/ServiceHealthCollector.java
+++ register/src/main/java/com/quang/serv/register/health/ServiceHealthCollector.java
@@ -2,17 +2,12 @@ package com.quang.serv.register.health;
 
 import com.quang.serv.core.components.Collector;
 import com.quang.serv.core.health.HealthReport;
-import com.quang.serv.pipeline.health.server.HealthServerBootstrap;
 
 /**
  * 集群健康度收集
  * @author Lianquan Yang
  */
 public class ServiceHealthCollector implements Collector<HealthReport> {
-    public void init(){
-        // 启动健康上报服务器
-        HealthServerBootstrap.start(this);
-    }
 
     /**
      * 健康度收集
